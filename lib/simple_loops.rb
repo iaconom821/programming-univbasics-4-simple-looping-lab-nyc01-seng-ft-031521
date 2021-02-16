08568b0047b3c8c78b196b9fa9ee239a888cbd1e
Michael Iacono <iaconom821@gmail.com>
2021-02-16 16:04:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,28 @@ def loop_message_five_times(string)
     count += 1 
   end
 end
+
+def loop_message_n_times(string, n)
+  count = 0 
+  while count <= n do
+    puts(string)
+    count += 1 
+  end
+end
+
+def output_array(array)
+  count = 0 
+  while array[count] do
+    puts array[count]
+    count += 1 
+  end
+end
+
+def return_string_array(array)
+  new_array = []
+  count = 0 
+  while array[count] do 
+    new_array.push(array[count].to_s)
+  end
+  new_array
+end
